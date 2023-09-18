1b603c3568baa860449dab706c288fe61b54d3c6
unknown <mohme.m312@gmail.com>
2023-09-18 11:57:08 -0400

    update app id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-// import DVSOIDVC from "@idscan/onboarding";
-import DVSOIDVC from "scandoc-onboarding";
+import DVSOIDVC from "@idscan/onboarding";
+// import DVSOIDVC from "scandoc-onboarding";
 import "scandoc-onboarding/dist/css/onboarding.css";
 
 class App extends Component {
@@ -12,15 +12,11 @@ class App extends Component {
   componentDidMount() {
     const lib = new DVSOIDVC({
       el: "videoCapturingEl",
-      applicantId: "3573220d-3ae0-499b-88db-2b828e400357",
+      applicantId: "2ec310eb-c816-4114-956e-071f4776c1a9",
       domainId: "c4091080-1c30-4c73-49b7-08dbb14e8517",
       publicKey: "pk_274eb098-b9df-43f5-84d2-d48d389bfc0e",
       chunkPublicPath: "networks",
       domainApi: "https://api-dvsonline.idscan.net",
-
-      // applicantId: "289c37ce-023a-4eca-9d64-9a809c9c2518",
-      // domainId: "466339a8-32a6-444e-5b28-08dbb0aead03",
-      // publicKey: "pk_93d3442d-251e-4a31-93e0-17b7099b3c6f",
     });
   }
 
