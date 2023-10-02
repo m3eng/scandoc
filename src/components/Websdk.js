e80f7bde9ab3438396b93b1841fece7ba3e58d20
unknown <mohme.m312@gmail.com>
2023-10-02 10:30:34 -0400

    qr code

---
+++ src/components/Websdk.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import ScanWrap from "scandoc-onboarding";
+import "scandoc-onboarding/dist/css/onboarding.css";
+
+function Websdk() {
+  useEffect(() => {
+    const lib = new ScanWrap({
+      el: "videoCapturingEl",
+      domainApi: process.env.REACT_APP_DOMAIN_API,
+      customerKey: process.env.REACT_APP_CUSTOMER_KEY,
+      domainId: process.env.REACT_APP_DOMAIN_ID,
+      publicKey: process.env.REACT_APP_PUBLIC_KEY,
+      configApi: process.env.REACT_APP_CONFIG_API,
+    });
+  });
+  return (
+    <div className="App">
+      <div id="videoCapturingEl"></div>
+    </div>
+  );
+}
+
+export default Websdk;
